1202fed5d240c26ad5f7c76c4dbc28c960391c08
bhawnaniankit <bhawnaniankit@gmail.com>
2024-04-13 23:02:39 +0530

    Bug fixed

--- src/utills/getTime.ts
+++ src/utills/getTime.ts
@@ -1,4 +1,4 @@
-export function getTime(date: Date) {
+export function getTime(date: any) {
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var now = new Date(date);
     return (months[now.getMonth()] + ' ' + now.getDate() + ',' + now.getFullYear());
