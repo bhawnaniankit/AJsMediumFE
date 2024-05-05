f07e582b4f4e72f8644fd46f97b6001726a7a921
Ankit Bhawnani <bhawnaniankit@gmail.com>
2024-05-05 15:12:20 +0530

    bug fixed

--- src/components/BlogCompo.tsx
+++ src/components/BlogCompo.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import InidiBlog from "./Blog";
 import { useParams } from "react-router-dom";
 import {useBlog} from "../hooks/useBlog";
+import { CgProfile } from "react-icons/cg";
 
 const BlogCompo = () => {
     const { id } = useParams();
@@ -10,6 +11,29 @@ const BlogCompo = () => {
         id: id || ""
     });
 
+    if (loading) {
+      return <>
+              <div className=" animate-pulse flex-col md:flex-row flex gap-8 mx-10 my-6 md:mx-44 md:my-10">
+                  <div className=" md:w-2/3">
+                      <div className=" rounded-md bg-gray-200 h-24 my-2"></div>
+                      <div className=" rounded-md bg-gray-200 h-4 my-3"></div>
+                      <div className=" rounded-md bg-gray-200 min-h-96 "></div>
+                  </div>
+                  <div className=" animate-pulse flex flex-col gap-1">
+                      <div className=" font-semibold">Author:</div>
+                      <div className=" items-start flex gap-2">
+                          <div className=" m-1">
+                              <CgProfile></CgProfile>
+                          </div>
+                          <div>
+                              <div className=" rounded-md bg-gray-200 h-96"></div>
+                              <div className=" rounded-md bg-gray-200 h-4"></div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+           </>
+  }
   return (
     <div>
         <div className=" flex-col md:flex-row flex gap-8 mx-10 my-6 md:mx-44 md:my-10">
